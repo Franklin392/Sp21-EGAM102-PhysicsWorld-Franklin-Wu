a031dbaed441e21619dfbef597cdf6b47cc70376
Franklin392 <FranklinSRT392@users.noreply.github.com>
2021-01-25 17:51:24 -0800

    first foundation avatar controller

---
+++ Assets/script/AvatarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarController : MonoBehaviour
+{
+    public float MovementStrength;
+    public float jumpSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigidbody2D LS;
+            LS = GetComponent<Rigidbody2D>();
+            LS.velocity = (Vector2.up * jumpSpeed);
+        }
+
+    }
+    private void FixedUpdate()
+    {
+          if(Input.GetKey(KeyCode.D)) 
+            {
+            Rigidbody2D HEMI;
+            HEMI = GetComponent<Rigidbody2D>();
+            HEMI.AddForce(MovementStrength * Vector3.right,ForceMode2D.Force);
+            }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Rigidbody2D HEMI;
+            HEMI = GetComponent<Rigidbody2D>();
+            HEMI.AddForce(MovementStrength * Vector3.left, ForceMode2D.Force);
+        }
+     
+        
+        }
+    }
+
