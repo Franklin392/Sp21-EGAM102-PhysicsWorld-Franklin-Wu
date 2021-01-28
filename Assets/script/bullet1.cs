f387c0f4fd95fdcb71409bb6825c7f1919e39566
Franklin392 <FranklinSRT392@users.noreply.github.com>
2021-01-27 17:05:49 -0800

    shooting skill add

---
+++ Assets/script/bullet1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet1 : MonoBehaviour
+{
+    Rigidbody2D AK47;
+    void Start()
+    {
+        AK47 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        AK47.AddForce(new Vector2(2, 0));
+    }
+}
---
+++ Assets/script/shoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour
+{
+    Object bullet556;
+    void Start()
+    {
+        bullet556 = Resources.Load("bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = (GameObject)Instantiate(bullet556);
+            bullet.transform.position = new Vector3(transform.position.x + .4f, transform.position.y + .2f, -1);
+        }
+    }
+}
