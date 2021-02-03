c0df6d3ef28049260315b0e7db6020ea0e52f502
Franklin392 <FranklinSRT392@users.noreply.github.com>
2021-02-03 02:27:11 -0800

    Base Requirements accomplish

---
+++ Assets/script/Star.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    public float XMin, XMax, YMin, Ymax;
+    public int NClones;
+    public float CloneScaleMin, CloneScaleMax;
+    public bool RandomizeSpriteColors;
+    void Start()
+    {
+        GameObject TYPE56;
+        for (int RedStar = 0; RedStar < NClones; RedStar++)
+        {
+            TYPE56 = Instantiate(ObjectToClone, this.transform);
+            TYPE56.transform.position = new Vector3(
+                 Random.Range(XMin, XMax),
+                 Random.Range(YMin, Ymax), 1f);
+            TYPE56.transform.localScale = Random.Range(CloneScaleMin, CloneScaleMax) * Vector3.one;
+
+            if (RandomizeSpriteColors)
+            {
+                TYPE56.GetComponent<SpriteRenderer>().color = Random.ColorHSV(0, 1,
+              0, .3f,
+              .7f, .9f,
+              .3f, .5f);
+            }
+
+        }
+    }
+}
