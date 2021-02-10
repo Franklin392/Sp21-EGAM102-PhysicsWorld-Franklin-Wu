1b8c0ac2c2abd6d83d4194e7606097e687dd8096
Franklin392 <FranklinSRT392@users.noreply.github.com>
2021-02-09 19:43:41 -0800

    part 3 update 2

---
+++ Assets/script/SquishyBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SquishyBox : MonoBehaviour
+{
+    public Transform[] CornerObjects;
+
+    private SpriteShapeController SpriteShapeController;
+    void Start()
+    {
+        SpriteShapeController = GetComponent<SpriteShapeController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpriteShapeController.spline.SetPosition(0, CornerObjects[0].position);
+        SpriteShapeController.spline.SetPosition(0, CornerObjects[1].position);
+        SpriteShapeController.spline.SetPosition(0, CornerObjects[2].position);
+        SpriteShapeController.spline.SetPosition(0, CornerObjects[3].position);
+    }
+}
